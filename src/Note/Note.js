5505ed2bf86b489616b22afff0aafcd26f196da0
Joshua Vaughan <jvaughan007@yahoo.com>
2020-11-16 12:39:55 -0600

    final turn-in client

---
+++ src/Note/Note.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { format } from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ApiContext from "../ApiContext";
+import config from "../config";
+import "./Note.css";
+
+export default class Note extends React.Component {
+  static defaultProps = {
+    onDeleteNote: () => {}
+  };
+  static contextType = ApiContext;
+
+  handleClickDelete = e => {
+    e.preventDefault();
+    const noteId = parseInt(this.props.id);
+    console.log(typeof noteId);
+    console.log(noteId);
+
+    fetch(`${config.API_ENDPOINT}/api/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) return res.json().then(e => Promise.reject(e));
+        return res;
+      })
+      .then(() => {
+        this.context.deleteNote(noteId);
+        // allow parent to perform extra behaviour
+        this.props.onDeleteNote(noteId);
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  };
+
+  render() {
+    const { name, id, modified } = this.props;
+    console.log(id);
+    console.log(this.props);
+    return (
+      <div className="Note">
+        <h2 className="Note__title">
+          <Link to={`/note/${id}`}>{name}</Link>
+        </h2>
+        <button
+          className="Note__delete"
+          type="button"
+          onClick={this.handleClickDelete}
+        >
+          <FontAwesomeIcon icon="trash-alt" /> remove
+        </button>
+        <div className="Note__dates">
+          <div className="Note__dates-modified">
+            Modified{" "}
+            <span className="Date">{format(modified, "Do MMM YYYY")}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/notes-helpers.js
@@ -0,0 +1,13 @@
+export const findFolder = (folders = [], folderId) =>
+  folders.find(folder => folder.id === folderId);
+
+export const findNote = (notes = [], noteId) =>
+  notes.find(note => note.id === noteId);
+
+export const getNotesForFolder = (notes = [], folderId) =>
+  !folderId ? notes : notes.filter(note => note.folderId === folderId);
+
+export const countNotesForFolder = (notes = [], folderId) => {
+  console.log(notes, folderId);
+  return notes.filter(note => note.folderId === folderId).length;
+};
